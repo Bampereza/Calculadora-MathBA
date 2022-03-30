1abed9015c26e14701132a0f5d1324685cddae38
Brandon <bampereza@miumg.edu.gt>
2022-03-30 14:59:37 -0600

    CalculadoraB

---
+++ CalculadoraB.java
@@ -0,0 +1,70 @@
+//Brandon Isaias Ampérez Aldana 0909-21-4628
+package calculadorab;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aldan
+ */
+public class CalculadoraB {
+    /**
+     * @param args the command line arguments
+     */
+  
+    public static void main(String[] args) {
+         System.out.println("--------------------------------------------------");
+         System.out.println("-----Bienvenido al programa de metodos MathBA-----");
+         System.out.println("--------------------------------------------------");
+         System.out.println("\n");
+         
+         Scanner sc= new Scanner(System.in);
+         
+         //Funcion de la raiz
+         
+         //System.out.println("Ingrese el numero que desea sacarle raiz cuadrada: ");
+         //double primerNumero = sc.nextDouble();
+         
+          //CalculadoraBA calculadoraB = new CalculadoraBA();
+          //System.out.println(calculadoraB.raizBA(primerNumero) );
+          //System.out.println("El resultado de la raiz es:  "+calculadoraB.raiz);
+          
+          
+          
+          
+         //Funcion de la potencia
+          
+         //System.out.println("Ingrese el Primer Numero: ");
+         //int primerNumero = sc.nextInt();
+         
+         //System.out.println("Ingrese el Segundo Numero: ");
+         //int segundoNumero = sc.nextInt();
+          
+          //CalculadoraBA calculadoraB = new CalculadoraBA();
+          //System.out.println(calculadoraB.potenciaBA(primerNumero, segundoNumero));
+          //System.out.println("El resultado de la potencia es:  "+calculadoraB.potencia);
+          
+          
+          
+          
+          //Funcion de redondear
+          
+         //System.out.println("Ingrese un Numero: ");
+         //double primerNumero = sc.nextDouble();
+          
+          //CalculadoraBA calculadoraB = new CalculadoraBA();
+          //System.out.println(calculadoraB.redondearBA(primerNumero));
+          //System.out.println("El redondeo de la aproximacion es de:  "+calculadoraB.redondear);
+          
+        
+          
+          
+          //Funcion numero aleatorio
+         
+            //CalculadoraBA calculadoraB = new CalculadoraBA();
+            //System.out.println(calculadoraB.numeroAleatorioBA());
+            //System.out.println("El numero creado al azar fue:  "+calculadoraB.numeroAleatorio);
+         
+    }
+    
+}
